feeb70a4eac11c7cfbdeaca15cefe0a2e0eccc4d
github username <github email address>
2020-04-24 23:44:10 -0400

    set up PlayerController to start working on PATCH requests from front end

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,11 @@
 class PlayersController < ApplicationController
+    def show
+        set_player
+        render json: PlayerSerializer.new(@player)
+    end
+
+    private
+        def set_player
+            @player = Player.find(params[:id])
+        end
 end
