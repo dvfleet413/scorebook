7210616bea63605a9d25694fef5b063c1142b259
github username <github email address>
2020-04-24 21:52:32 -0400

    rendering json at /teams/:id endpoint

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,12 @@
 class TeamsController < ApplicationController
+    def show 
+        set_team
+        render json: @team, include: [:players]
+    end
+
+
+    private
+        def set_team
+            @team = Team.find(params[:id])
+        end
 end
