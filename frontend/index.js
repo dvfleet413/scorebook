231cea09d2cc152eb36e3083925f5cb23df76721
github username <github email address>
2020-04-24 23:28:37 -0400

    started working on JS classes

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,40 @@
-const title = document.querySelector('h1');
-title.innerText = 'testing that index.js is loaded'
+class Team {
+    constructor(name){
+        this._name = name;
+    }
+}
+
+class Player {
+    constructor(name, number, position){
+        this._name = name;
+        this._number = number;
+        this._position = position;
+    }
+}
+
+class Game {
+    constructor(currentInning = 1.0){
+        this._currentInning = currentInning;
+    }
+
+    changeSides(){
+        this._currentInning += 0.5;
+    }
+}
+
+class AtBat {
+    constructor(batter, result, baseReached, outNumber){
+        this._batter = batter;
+        this._result = result;
+        this.baseReached = baseReached;
+        this._outNumber = outNumber;
+    }
+
+    advanceToBase(baseReached){
+        this.baseReached = baseReached;
+    }
+
+    score(){
+        this.baseReached = 4;
+    }
+}
