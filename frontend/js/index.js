6713c7ca0885b094b7894bb58abe2868f0f25ee0
github username <github email address>
2020-04-30 00:33:44 -0400

    index page renders links for each game ifrom the GET request to /games

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -14,17 +14,50 @@ const run = function(){
         currentGame.start()
     })
     App.appendToMain(newGameBtn)
+    getGameList()
 }
+
+const getGameList = function(){
+    const url = 'http://localhost:3000/games'
+    fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(json => {
+            const gameList = document.createElement('div')
+            gameList.setAttribute('id', 'game-list')
+            App.appendToMain(gameList)
+            console.log(json['data'])
+            json['data'].forEach(game => {
+                const gameLi = document.createElement('li')
+                const gameLink = document.createElement('a')
+                gameLink.setAttribute('href', '#')
+                gameLink.setAttribute('id', `${game.id}-1`)
+                gameLink.addEventListener('click', handleClick)
+                gameLink.innerText = game['attributes']['created_at']
+                gameLi.appendChild(gameLink)
+                gameList.appendChild(gameLi)
+            })
+            console.log(json)
+        })
+}
+
+const handleClick = (e) => {
+    e.preventDefault()
+    console.log(e)
+}
+
+
 
 run()
 
-Game.requestSavedGame(currentGame, 2)
-    .then((response) => {
-        currentGame = response
-        currentGame.summarize()
-    })
-    .catch((error) => {
-        console.log(error.message)
-    })
+// Game.requestSavedGame(currentGame, 2)
+//     .then((response) => {
+//         currentGame = response
+//         currentGame.summarize()
+//     })
+//     .catch((error) => {
+//         console.log(error.message)
+//     })
 
 
