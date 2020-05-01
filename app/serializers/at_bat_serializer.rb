005366df82bef73caa28460bf9511232fd4b24aa
github username <github email address>
2020-04-30 23:50:18 -0400

    refactoring

--- app/serializers/at_bat_serializer.rb
+++ app/serializers/at_bat_serializer.rb
@@ -3,5 +3,4 @@ class AtBatSerializer
   set_key_transform :camel_lower
   attributes :player, :result, :base_reached, :out_number, :out_code
   belongs_to :inning
-  
 end
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -10,7 +10,7 @@ newGameBtn.addEventListener('click', (e) => {
     currentGame.start()
 })
 
-const getGameList = function(){
+const getGameList = () => {
     const url = 'http://localhost:3000/games'
     fetch(url)
         .then(response => {
@@ -19,23 +19,16 @@ const getGameList = function(){
         .then(json => {
             const gameList = document.getElementById('game-list')
             json['data'].forEach(game => {
-                const gameLi = document.createElement('li')
-                const gameLink = document.createElement('a')
-                gameLink.setAttribute('href', '#')
-                gameLink.setAttribute('id', `${game.id}-game`)
-                gameLink.addEventListener('click', handleClick)
-                console.log(game)
-                gameLink.innerText = game['attributes']['createdAt']
-                gameLi.appendChild(gameLink)
-                gameList.appendChild(gameLi)
+                gameList.innerHTML += `<li><a href="#" id="${game.id}-game" onclick="handleClick()">${game['attributes']['createdAt']}</a></li>`
             })
-            console.log(json)
         })
-}()
+}
 
-const handleClick = (e) => {
-    e.preventDefault()
-    const id = parseInt(e.target.id, 10)
+getGameList()
+
+const handleClick = () => {
+    event.preventDefault()
+    const id = parseInt(event.target.id, 10)
     Game.requestSavedGame(currentGame, id)
         .then(response => {
             currentGame = response
@@ -45,3 +38,4 @@ const handleClick = (e) => {
             console.log(error.message)
         })
 }
+window.handleClick = handleClick;
